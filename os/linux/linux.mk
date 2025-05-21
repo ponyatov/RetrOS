f83b966f3d0b08af86753be3403ad8a669649bec
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-21 12:33:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- os/linux/linux.mk
+++ os/linux/linux.mk
@@ -9,16 +9,16 @@ GZ += ref/busybox-$(BB_VER)/README
 ref/busybox-$(BB_VER)/README:
 	cd ref ; apt source busybox
 
-LINUX_MK = ref/linux-$(LINUX_VER)/Makefile
+LINUX_MK = $(CWD)/ref/linux-$(LINUX_VER)/Makefile
 GZ += $(LINUX_MK)
 $(LINUX_MK):
 	cd ref ; apt source linux
 endif
 
-LINUX_CFG += hw/all.linux
-LINUX_CFG += arch/$(ARCH)/$(ARCH).linux
-LINUX_CFG += cpu/$(CPU)/$(CPU).linux
-LINUX_CFG += hw/$(HW)/$(HW).linux
+LINUX_CFG += $(CWD)/hw/all.linux
+LINUX_CFG += $(CWD)/arch/$(ARCH)/$(ARCH).linux
+LINUX_CFG += $(CWD)/cpu/$(CPU)/$(CPU).linux
+LINUX_CFG += $(CWD)/hw/$(HW)/$(HW).linux
 
 LINUX_MAKE  = $(MAKE) -f $(LINUX_MK)
 LINUX_MAKE += ARCH=$(ARCH) CROSS_COMPILE=$(LINUX_TARGET)-
@@ -29,7 +29,12 @@ LINUX_MAKE += INSTALL_DTBS_PATH=$(ROOT)/dtbs
 
 .PHONY: linux
 linux: tmp/linux/.config $(LINUX_MK)
+	cd tmp/kernel ;\
+	$(LINUX_MAKE) menuconfig && $(LINUX_MAKE) -j2 &&\
 
 tmp/linux/.config: $(LINUX_CFG) mk/cross.mk os/linux/linux.mk
-	mkdir -p tmp/linux ; cd tmp/linux ;\
-	rm -f .config 
+	mkdir -p $(dir $@) ; cd $(dir $@) ;\
+	rm -f .config ; $(LINUX_MAKE) allnoconfig ;\
+	cat $(LINUX_CFG) >> .config ;\
+	echo 'CONFIG_LOCALVERSION="-$(HW)"'        >> .config ;\
+	echo 'CONFIG_DEFAULT_HOSTNAME="$(MODULE)"' >> .config
